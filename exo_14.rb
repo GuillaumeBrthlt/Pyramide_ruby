cb62847f109bf302fd2570d371315112dc60d942
GuillaumeBrthlt <bertholetguillaume@gmail.com>
2022-07-06 19:07:06 +0200

    mise à jour

--- exo_14.rb
+++ exo_14.rb
@@ -1,5 +1,8 @@
 mails = []
-(1..50).select(&:even?).each do |i|
-  puts "jean.dupont.%02d@email.fr" %[i]
-  mails.push(i)
+(1..50).each do |i|
+ mails[i] = "jean.dupont.%02d@email.fr" %[i]
+ if i.even?
+  puts mails[i]
+ end
 end
+
